cc3146516c22be0eafaa7221d8d473772f5caf4b
Chris Feldmeier <chris.feldmeier@solife.me>
2014-02-15 21:46:18 +0100

    basegen v1.1

--- basegen.sh
+++ basegen.sh
@@ -1,16 +1,16 @@
-//
-//  Basegen
-//  Generator for Beaconbase UUIDs
-//
-//  Created by Chris Feldmeier on 15/2/14.
-//  Copyright (c) 2014 Beaconbase. All rights reserved.
-//
 #!/bin/sh
+#
+#  Basegen
+#  Generator for Beaconbase UUIDs
+#
+#  Created by Chris Feldmeier on 15/2/14.
+#  Copyright (c) 2014 Beaconbase. All rights reserved.
+#
 
-// generate system random uuid for beaconbase.eu Beacons
-echo "Beacon UUID: `uuidgen -r`";
+# generate system random uuid for beaconbase.eu Beacons
+echo "Beacon UUID: `uuidgen`";
 
-// add major, minor id
+# add major, minor id
 read -p "Do you want to add major, minor ID? (y/n) " RESP
 if [ "$RESP" = "y" ]; then
   echo "";
